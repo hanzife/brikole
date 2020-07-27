3693547b84b5f535c533f827b5854e32eeb10481
AHmims <ali.hmims99@gmail.com>
2020-07-27 08:17:28 +0100

    on resize sp display control

--- project-draft/js/searchResult.js
+++ project-draft/js/searchResult.js
@@ -98,13 +98,58 @@ $(".sr-profile-fav").click((e) => {
 // Beacause i'm lazy, mafiyach li ybdl f'html o nzid les attributes manualy, soooo
 // JS to automate that
 // // add Sousprofession value as a attribute to it's prespective tag (language 100)
-$('.sr-SousProf').each((i, e) => {
-    // Extra :: just to beautify the text
-    $(e).text($(e).text().trim());
-    // Set attributes
-    $(e).attr('data-value', $(e).text());
-    $(e).attr('data-size', e.clientWidth);
-});
+load_attribs_data();
+
+function load_attribs_data() {
+    // let sizes = [];
+    // if (window.innerWidth <= 575) {
+    //     $('.sr-profileInfo').each((i_s, cont) => {
+    //         const sub_cont = cont.children[1];
+    //         // In small sizes the element of whoch i get the size data is display none
+    //         // which means getting size values will result in 0
+    //         // so the trick is to redesplay it, hide it, position absllute it to not interfare with the layout
+    //         sub_cont.style.visibility = 'hidden';
+    //         sub_cont.style.display = 'flex';
+    //         sub_cont.style.position = 'absolute';
+    //         $(sub_cont.children).each((i_e, sp) => {
+    //             sp.style.fontSize = '14px';
+    //             // 
+    //             sizes.push(sp.clientWidth);
+    //         });
+    //     });
+    //     // console.log(sizes);
+    // }
+    // //
+    // let sizes_pos = 0;
+    $('.sr-SousProf').each((i, e) => {
+        // Extra :: just to beautify the text
+        $(e).text($(e).text().trim());
+        // Set attributes
+        $(e).attr('data-value', $(e).text());
+        // if (window.innerWidth <= 575) {
+        //     if ($(e).parent().hasClass('sr-SousProf-all-resp')) {
+        //         // console.log(e, sizes[sizes_pos]);
+        //         $(e).attr('data-size', sizes[sizes_pos]);
+        //         sizes_pos++;
+        //     }
+        // } else
+        $(e).attr('data-size', e.clientWidth);
+    });
+    // 
+    // if (window.innerWidth <= 575) {
+    //     $('.sr-profileInfo').each((i_s, cont) => {
+    //         const sub_cont = cont.children[1];
+    //         // Reset style
+    //         sub_cont.style.visibility = 'visible';
+    //         sub_cont.style.display = 'none';
+    //         sub_cont.style.position = 'unset';
+    //         $(sub_cont.children).each((i_e, sp) => {
+    //             sp.style.fontSize = '12px';
+    //         });
+    //     });
+    // }
+
+}
 // handling sous-profession display
 adaptive_sps();
 window.addEventListener("resize", adaptive_sps);
@@ -135,39 +180,17 @@ function adaptive_sps() {
                 cont_inner_width += sous_profession.clientWidth + parseFloat(sp_style.marginRight.substr(0, sp_style.marginRight.length - 2));
             }
             // 
-            // console.group();
-            // let phase_done = false;
             while (cont_inner_width > cont_width) {
-                // console.group();
-                // console.log({
-                //     cont_inner_width,
-                //     cont_width,
-                //     last_sp_child
-                // });
                 cont_inner_width -= sp_cont.children[last_sp_child].clientWidth;
                 sp_cont.children[last_sp_child].innerText = '...';
                 sp_cont.children[last_sp_child].style.marginRight = "0px";
                 // 
-                if (cont_inner_width > cont_width) {
+                if (cont_inner_width > cont_width)
                     sp_cont.children[last_sp_child].style.visibility = 'hidden';
-                    // 
-                }
-                // else
-                // backwards();
                 last_sp_child--;
-                // console.log({
-                //     cont_inner_width,
-                //     cont_width,
-                //     last_sp_child
-                // });
-                // console.groupEnd();
             }
-            let ttt_data = {
-                pos: i,
-                cont_inner_width,
-                cont_width
-            }
-            if (cont_inner_width < cont_width) {
+            // 
+            if (cont_inner_width < cont_width && windowSize > 575) {
                 let dots_pos = -1;
                 for (let j = 0; j < sp_cont.children.length; j++) {
                     if (sp_cont.children[j].innerText == '...') {
@@ -175,37 +198,23 @@ function adaptive_sps() {
                         j = sp_cont.children.length;
                     }
                 }
-                // 
-                ttt_data.dot_pos = dots_pos;
-                ttt_data.child_length = sp_cont.children.length - 1;
-                // 
                 if (dots_pos != -1) {
-                    ttt_data.childs = [];
                     for (let j = dots_pos; j < sp_cont.children.length; j++) {
                         const sp_child = sp_cont.children[j];
                         const sp_child_width = sp_child.clientWidth;
                         const sp_child_orig_width = parseFloat(sp_child.getAttribute('data-size'));
-                        // const sp_child_style = window.getComputedStyle(sp_child);
-                        // const sp_child_marginRight = parseFloat(sp_child_style.marginRight.substr(0, sp_child_style.marginRight.length - 2));
-                        // 
-                        ttt_data.childs.push({
-                            j,
-                            sp_child_width,
-                            // sp_child_marginRight,
-                            sp_child_orig_width
-                        });
                         // 
                         let marginValue = windowSize <= 575 ? 1 : 5;
                         // 
                         let next_block = j < sp_cont.children.length - 1 ? sp_cont.children[j + 1].clientWidth : 0;
                         // 
-                        console.log((cont_inner_width - sp_child_width) + marginValue + sp_child_orig_width + next_block, cont_width);
-                        console.log({
-                            minus: cont_inner_width - sp_child_width,
-                            marginValue,
-                            sp_child_orig_width,
-                            next_block
-                        }, cont_width)
+                        // if (i == 0)
+                        // console.log((cont_inner_width - sp_child_width) + marginValue + sp_child_orig_width + next_block, {
+                        //     minus: cont_inner_width - sp_child_width,
+                        //     marginValue,
+                        //     sp_child_orig_width,
+                        //     next_block
+                        // }, cont_width)
                         if ((cont_inner_width - sp_child_width) + marginValue + sp_child_orig_width + next_block <= cont_width) {
                             sp_child.innerText = sp_child.getAttribute('data-value');
                             if (sp_cont.children[j + 1]) {
@@ -216,60 +225,10 @@ function adaptive_sps() {
                             }
                             cont_inner_width = (cont_inner_width - sp_child_width) + sp_child_orig_width;
                         }
-                        // console.log(true);
-                        // console.log({
-                        //     i,
-                        //     sp_child_width,
-                        //     sp_child_marginRight
-                        // });
                     }
-                    // console.log(ttt_data);
                 }
-                // 
-                // if (i == 0)
-                // console.log(ttt_data);
             }
-            // 
-            /*if (cont_inner_width < cont_width) {
-                // while (cont_inner_width < cont_width) {
-                let dots_pos = -1;
-                for (let j = 0; j < sp_cont.children.length; j++) {
-                    if (sp_cont.children[j].innerText == '...') {
-                        dots_pos = j;
-                        j = sp_cont.children.length;
-                    }
-                }
-                if (dots_pos > -1) {
-                    for (let x = dots_pos; x < sp_cont.children.length; x++) {
-                        const element = sp_cont.children[x];
-                        let element_old_size = element.clientWidth;
-                        // 
-                        let next_elem_size = sp_cont.children[x + 1] != undefined ? $(sp_cont.children[x + 1]).innerWidth() : 0;
-                        var element_style = window.getComputedStyle(element);
-                        // 
-                        let testData = {
-                            pos: i,
-                            size: (cont_inner_width - element_old_size) + parseFloat(element.getAttribute('data-size')) + next_elem_size - parseFloat(element_style.marginRight.substr(0, element_style.marginRight.length - 2)),
-                            cont: cont_width,
-                            old: element_old_size
-                        }
-                        console.log(testData);
-                        // console.log(i, , cont_width);
-                        if ((cont_inner_width - element_old_size) + parseFloat(element.getAttribute('data-size')) + next_elem_size < cont_width) {
-                            // console.log('in');
-                            element.innerText = element.getAttribute('data-value');
-                            if (sp_cont.children[x + 1])
-                                sp_cont.children[x + 1].style.display = 'block';
-                            cont_inner_width = (cont_inner_width - element_old_size) + parseFloat(element.getAttribute('data-size'));
-                            // console.log(cont_inner_width, cont_width, dots_pos);
-                        }
-                    }
-                }
-
-            }*/
-            // console.groupEnd();
         }
-        // console.log(sp_cont);
     });
     if (windowSize <= 575) {
 
